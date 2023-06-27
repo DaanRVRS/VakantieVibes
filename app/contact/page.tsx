c9f004da336c13a71812446272f7198ea017e677
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2023-06-27 23:28:02 +0200

    gg

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -52,7 +52,7 @@ const Contact = () => {
           <div className="bg-gray-300 border-2 border-black h-48 w-44 my-5 mx-12">
             <h1 className="text-black"> Bagage</h1>
           </div>
-        </section>git 
+        </section>git git 
             <h1 className="text-black"> Andere vragen</h1>
           </div> */}
         </section>
